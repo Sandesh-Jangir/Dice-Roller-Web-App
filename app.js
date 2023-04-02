15eef7d6713ee1e0bacb4aeae657f874a60c073b
Sandesh Jangir <sandeshatgithub@gmail.com>
2023-04-02 19:25:33 +0530

    Created the js function to generate function.

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Checking JS Engine ...")
+// Function to generate random integer between 1 - 6.
+const generateDiceNumber = ()=>{
+    let number = Math.floor(Math.random()*6) + 1;
+    return number;
+}
+
+console.log(generateDiceNumber()); // Printing the result
